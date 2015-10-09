80bb0fe079311e6bdcd6946e52dfb258dcc36f8b
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:58:25 -0400

    changed table name

--- db/migrate/20151009152459_create_user_story.rb
+++ db/migrate/20151009152459_create_user_story.rb
@@ -1,6 +1,6 @@
 class CreateUserStory < ActiveRecord::Migration
   def change
-    create_table :user_stories do |t|
+    create_table :user_surveys do |t|
       t.belongs_to :user
       t.belongs_to :survey
     end
