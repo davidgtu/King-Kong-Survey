1b903dc6357c540169b7a770cae8fab141bfa489
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:38:39 -0400

    fix typo in repsonse

--- db/migrate/20151009151845_create_responses.rb
+++ db/migrate/20151009151845_create_responses.rb
@@ -2,7 +2,7 @@ class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
       t.belongs_to :user
-      t.belongs_to :choices
+      t.belongs_to :choice
 
       t.timestamps null:false
     end
