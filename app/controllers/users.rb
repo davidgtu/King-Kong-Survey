2f82fc973d26c644c366dea6315fa86814e96c89
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 16:35:51 -0400

    add post route for registering

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,3 +2,15 @@ get '/register' do
   @user = User.new
   erb :'/users/new'
 end
+
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    #flash message for success
+    redirect '/'
+  else
+    #flash message for error
+    redirect '/'
+  end
+end
