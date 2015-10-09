730f34318d69b4f848a3bb491819cc906689785f
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:52:35 -0400

    add through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :user_surveys
+  has_many :surveys, through: :user_surveys, foreign_key: :author_id
   has_many :responses
-  has_many :surveys, foreign_key: :author_id
 end
