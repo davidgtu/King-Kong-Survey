a1ddf96f94b0008a0d1c42b1c47ea59df1640850
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:39:09 -0400

    add columns to surveys

--- db/migrate/20151009151939_create_surveys.rb
+++ db/migrate/20151009151939_create_surveys.rb
@@ -1,4 +1,11 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
+    create_table :surveys do |t|
+      t.string :title, null:false
+      t.string :about, null:false
+      t.belongs_to :author_id
+
+      t.timestamps null:false
+    end
   end
 end
