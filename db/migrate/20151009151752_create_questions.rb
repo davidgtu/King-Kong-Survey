062d4b4397737bd0a2a45b030c68bfefcbced9ed
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:28:18 -0400

    add columns to questions

--- db/migrate/20151009151752_create_questions.rb
+++ db/migrate/20151009151752_create_questions.rb
@@ -1,4 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string :prompt, null:false
+      t.belongs_to :survey
+
+      t.timestamps null:false
+    end
   end
 end
