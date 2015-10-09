e892a548add2823564bb55dfdb3242b04fa5ba9a
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:38:12 -0400

    fix typo

--- db/migrate/20151009151819_create_choices.rb
+++ db/migrate/20151009151819_create_choices.rb
@@ -2,7 +2,7 @@ class CreateChoices < ActiveRecord::Migration
   def change
     create_table :choices do |t|
       t.string :content, null:false
-      t.belongs_to :questions
+      t.belongs_to :question
 
       t.timestamps null:false
     end
