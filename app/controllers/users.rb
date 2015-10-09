ca653550fbb248865a21dd0a031e9e046409f41a
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 16:43:39 -0400

    add routes for login

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -14,3 +14,21 @@ post '/users' do
     redirect '/'
   end
 end
+
+get '/login' do
+  @user = User.new
+  erb :'/users/login'
+end
+
+post '/login' do
+  user_params = params[:user]
+  user = User.find_by(username: user_params[:username])
+  if user && user.password == user_params[:password]
+    session[:user_id] = user.id
+    #flash login success
+    redirect '/'
+  else
+    #flash login error
+    redirect '/login'
+  end
+end
