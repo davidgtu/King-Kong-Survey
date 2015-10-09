cdf5721f0f8a3a2630dde3787b1446a079af7745
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-09 15:29:58 -0400

    add columns to choices

--- db/migrate/20151009151819_create_choices.rb
+++ db/migrate/20151009151819_create_choices.rb
@@ -1,4 +1,10 @@
 class CreateChoices < ActiveRecord::Migration
   def change
+    create_table :choices do |t|
+      t.string :content, null:false
+      t.belongs_to :questions
+
+      t.timestamps null:false
+    end
   end
 end
