3e1565264d450f0051d67d131be9d367e81d16dc
Heather Lorenzo <email": null>
2015-10-10 14:35:51 -0400

    Moves the user log in and registering to the auth controller

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,7 +1,40 @@
+get '/register' do
+  @user = User.new
+  erb :'/users/new'
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    #flash message for success
+    redirect '/'
+  else
+    #flash message for error
+    redirect '/'
+  end
+end
+
 get '/login' do
-redirect '/'
+  @user = User.new
+  erb :'/users/login'
+end
+
+post '/login' do
+  user_params = params[:user]
+  user = User.find_by(username: user_params[:username])
+  if user && user.password == user_params[:password]
+    session[:user_id] = user.id
+    #flash login success
+    redirect '/'
+  else
+    #flash login error
+    redirect '/login'
+  end
 end
 
 get '/logout' do
-redirect '/'
+  session.clear
+  #flash msg for logout
+  redirect '/'
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,45 +1,5 @@
-get '/register' do
-  @user = User.new
-  erb :'/users/new'
-end
-
-post '/users' do
-  user = User.new(params[:user])
-  if user.save
-    session[:user_id] = user.id
-    #flash message for success
-    redirect '/'
-  else
-    #flash message for error
-    redirect '/'
-  end
-end
-
-get '/login' do
-  @user = User.new
-  erb :'/users/login'
-end
-
-post '/login' do
-  user_params = params[:user]
-  user = User.find_by(username: user_params[:username])
-  if user && user.password == user_params[:password]
-    session[:user_id] = user.id
-    #flash login success
-    redirect '/'
-  else
-    #flash login error
-    redirect '/login'
-  end
-end
-
 get '/users/:id' do
   @user = User.find(params[:id])
   erb :'/users/show'
 end
 
-get '/logout' do
-  session.clear
-  #flash msg for logout
-  redirect '/'
-end
