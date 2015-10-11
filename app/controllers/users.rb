4cbce454ddd7bd53947a62731fe4c43b0de2c1d0
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-11 19:06:32 -0400

    spacing

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -4,7 +4,6 @@ get '/users/:id' do
   else
     @user = User.find(params[:id])
     @surveys_written = Survey.where(author_id: @user.id)
-
     @surveys_taken = @user.responses.map do |response|
       response.get_question
     end.uniq!
