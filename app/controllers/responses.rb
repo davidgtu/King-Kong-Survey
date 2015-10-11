b11c98e62717c5273c6e3c05962c066136b42cc9
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-11 11:22:36 -0400

    add surveys response route

--- app/controllers/responses.rb
+++ app/controllers/responses.rb
@@ -1,5 +1,4 @@
 get '/surveys/:id/responses' do
-  @user = User.find(session[:user_id])
-  # @survey = @user.surveys.find(params[:id])
+  @survey = Survey.find(params[:id])  
   erb :'/responses/show'
 end
