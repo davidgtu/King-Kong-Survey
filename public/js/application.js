f63ff68262c7b8fe0ecf62732294d49e78130ee5
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-10 20:43:16 -0400

    Progress on ajax for creating new surveys

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $(".flash-success").fadeIn('slow', function(){
     $(this).delay(2000).fadeOut('slow')
   })
@@ -6,4 +7,22 @@ $(document).ready(function() {
   $(".flash-error").fadeIn('slow', function(){
     $(this).delay(2000).fadeOut('slow')
   })
+
+  var surveyForm = $("#new-survey");
+  surveyForm.on("submit", function(event) {
+    event.preventDefault();
+    var url = $(this).attr("action");
+    var data = $(this).serialize;
+    var type = $(this).attr("method");
+    $.ajax({
+      url: url,
+      data: data,
+      type: type
+    })
+    .done(function(data) {
+      debugger;
+    })
+    .fail();
+  } );
+
 });
